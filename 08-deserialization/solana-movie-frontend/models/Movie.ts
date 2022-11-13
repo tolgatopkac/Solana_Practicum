612d40614d51be5442092173a648d00161c2fe40
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-11-13 03:47:15 +0300

    movie deserialization

---
+++ 08-deserialization/solana-movie-frontend/models/Movie.ts
@@ -0,0 +1,50 @@
+import * as borsh from '@project-serum/borsh'
+
+export class Movie {
+    title: string;
+    rating: number;
+    description: string;
+
+    constructor(title: string, rating: number, description: string) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    // serialize
+    borshInstructionSchema = borsh.struct([
+        borsh.u8('variant'),
+        borsh.str('title'),
+        borsh.u8('rating'),
+        borsh.str('description'),
+    ])
+
+    // deserialize
+    static borshAccountSchema = borsh.struct([
+        borsh.bool('initialized'),
+        borsh.u8('rating'),
+        borsh.str('title'),
+        borsh.str('description'),
+    ])
+
+    serialize(): Buffer {
+        const buffer = Buffer.alloc(1000)
+        this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer)
+        return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer))
+    }
+
+    static deserialize(buffer?: Buffer): Movie | null {
+        if (!buffer) {
+            return null
+        }
+
+        try {
+            const { title, rating, description } = this.borshAccountSchema.decode(buffer)
+            return new Movie(title, rating, description)
+        } catch (e) {
+            console.log('Deserialization error:', e)
+            console.log(buffer)
+            return null
+        }
+    }
+}
